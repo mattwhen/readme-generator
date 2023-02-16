641ab9d43251424699b1122841b5e938f6501460
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-02-15 22:12:11 -0600

    this commit will add the inquirer package, user prompt, and write the content to the readme file

---
+++ Develop/README.md
@@ -0,0 +1,4 @@
+# Project Title
+ <br>
+## Description 
+ <br>
--- Develop/index.js
+++ Develop/index.js
@@ -1,17 +1,55 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer'); 
-console.log(inquirer);
+const fs = require('fs'); 
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
-  
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'projectTitle',
+    },
+    {
+        type: 'input',
+        message: 'Write a brief description of your project: ',
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: 'Which license would you like to use?',
+        name: 'license',
+        choices: ['MIT', 'GNU GPLv3', 'Apache License 2.0', 'GNU AGPLv3','GNU LGPLv3'],
+    },
 ];
 
+
+// Add the headers along with the user's input in the README file
+const content = `# Project Title
+ <br>
+## Description 
+ <br>`
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile() {
+    fs.writeFile('README.md', content, (error) =>  
+        error ? console.error(error) : console.log('Success!')
+    ); 
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile(); 
+}
 
+// Call the inquirer
+inquirer 
+    .prompt(questions) // Prompts users with questions and takes user input
+    .then((response) => {
+        return response;
+    })
+    .then((data) => {
+        console.log(data);
+    });
 // Function call to initialize app
 init();
+
